6fd3bc3c7a048d01bd61e47d511569905f3b8ffc
Ruemena <rue.heart.mena@gmail.com>
2024-06-17 23:14:06 -0500

    lol

---
+++ CmdbindFixer/Config.cs
@@ -0,0 +1,12 @@
+﻿namespace CmdbindFixer
+{
+#if EXILED
+    public sealed class Config : Exiled.API.Interfaces.IConfig
+#else
+    public sealed class Config
+#endif
+    {
+        public bool IsEnabled { get; set; } = true;
+        public bool Debug { get; set; } = true;
+    }
+}
---
+++ CmdbindFixer/Plugin.cs
@@ -0,0 +1,64 @@
+﻿namespace CmdbindFixer
+{
+#if EXILED
+    using Exiled.API.Features;
+    using Exiled.Events.EventArgs.Player;
+#else
+    using PluginAPI.Core;
+    using PluginAPI.Core.Attributes;
+    using PluginAPI.Enums;
+#endif
+
+    public class CmdbindFixer
+#if EXILED
+    : Plugin<Config>
+#endif
+    {
+        private static readonly UserGroup fixerGroup = new UserGroup
+        {
+            BadgeColor = "none"
+        };
+
+#if NWAPI
+#nullable disable
+        [PluginConfig] public Config Config;
+#nullable enable
+#endif
+
+#if EXILED
+        public override void OnEnabled()
+        {
+            Exiled.Events.Handlers.Player.Verified += OnPlayerVerified;
+        }
+#else
+        [PluginEntryPoint("CmdbindFixer", "1.0.0", "Fixes cmdbinds lol", "Rue <3")]
+        private void LoadPlugin()
+
+        {
+            if (!Config.IsEnabled)
+            {
+                return;
+            }
+
+            PluginAPI.Events.EventManager.RegisterEvents(this);
+        }
+#endif
+
+#if EXILED
+        private static void OnPlayerVerified(VerifiedEventArgs ev) {
+            Player player = ev.Player;
+#else
+        [PluginEvent(ServerEventType.PlayerJoined)]
+        public bool OnPlayerJoin(Player player)
+        {
+#endif
+            UserGroup current = ServerStatic.PermissionsHandler.GetUserGroup(player.ReferenceHub.authManager.UserId);
+            player.ReferenceHub.serverRoles.SetGroup(fixerGroup);
+            player.ReferenceHub.serverRoles.SetGroup(current);
+
+#if !EXILED
+            return true;
+#endif
+        }
+    }
+}
